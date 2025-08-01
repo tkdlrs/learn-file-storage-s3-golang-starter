3b68a292d95d652cee5b239639e00da74ded2534
tkdlrs <tkd.lsanchez@gmail.com>
2025-07-31 18:40:17 -0600

    Completes CH1: File Storage, L5: Multipart Uploads in the 'Learn File Servers and CDNs...' course on Boot.dev without looking at the solution files.

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/database"
 	"github.com/google/uuid"
 )
 
@@ -28,10 +30,54 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
+	//
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer file.Close()
+	//
+	mediaType := header.Header.Get("Content-Type")
+	//
+	imageData, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to get image data", err)
+	}
+	//
+	videoInDatabase, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Could not get video", err)
+		return
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	thumbnailURLTemplate := fmt.Sprintf("http://localhost:%v/api/thumbnails/%v ", cfg.port, videoID.String())
+	videoInDatabase.ThumbnailURL = &thumbnailURLTemplate
+	newVideoThumbnail := thumbnail{
+		data:      imageData,
+		mediaType: mediaType,
+	}
+
+	videoThumbnails[videoID] = newVideoThumbnail
+	cfg.db.UpdateVideo(videoInDatabase)
+	//
+	updatedVideo, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Could not get video", err)
+		return
+	}
+
+	//
+	respondWithJSON(w, http.StatusOK, database.Video{
+		ID:           updatedVideo.ID,
+		CreatedAt:    updatedVideo.CreatedAt,
+		UpdatedAt:    updatedVideo.UpdatedAt,
+		ThumbnailURL: updatedVideo.ThumbnailURL,
+		VideoURL:     updatedVideo.VideoURL,
+	})
 }
