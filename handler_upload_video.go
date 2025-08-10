c22dd518673aab4fa00ec4dded50c07f88fa96b7
tkdlrs <tkd.lsanchez@gmail.com>
2025-08-10 13:35:54 -0600

    Completes CH4: Object Storage, L3: Dynamic Path, of the 'Learn File Servers and CDNs' course on Boot.dev. I reached this conclusion without looking at the solution files. I will have to deep dive on the solution files they provided and confirm my understanding.

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"io"
+	"log"
 	"mime"
 	"net/http"
 	"os"
+	"os/exec"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
@@ -81,13 +86,28 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		respondWithError(w, http.StatusInternalServerError, "Unable to rewind video", err)
 		return
 	}
+	// Figure out if the video is horiztonal or vertical for filepath prefixing
+	videoAspectRatio, err := getVideoAspectRatio(tempFile.Name())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to get aspect ratio", err)
+		return
+	}
+	//
+	keyPrefix := "other/"
+	if videoAspectRatio == "16:9" {
+		keyPrefix = "landscape/"
+	}
+	if videoAspectRatio == "9:16" {
+		keyPrefix = "portrait/"
+	}
 	//
 	key := getAssetPath(mediaType)
-
+	//
+	keyWithPrefix := fmt.Sprintf("%s%s", keyPrefix, key)
 	// Prepare PutObjectInput
 	putObjectInput := &s3.PutObjectInput{
 		Bucket:      aws.String(cfg.s3Bucket),
-		Key:         aws.String(key),
+		Key:         aws.String(keyWithPrefix),
 		Body:        tempFile,
 		ContentType: aws.String(mediaType),
 	}
@@ -99,7 +119,7 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		return
 	}
 	//
-	url := cfg.getObjectURL(key)
+	url := cfg.getObjectURL(keyWithPrefix)
 	video.VideoURL = &url
 	err = cfg.db.UpdateVideo(video)
 	if err != nil {
@@ -108,4 +128,121 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 	}
 	//
 	respondWithJSON(w, http.StatusOK, video)
+}
+
+type FfprobeSteam struct {
+	Streams []struct {
+		Index              int    `json:"index"`
+		CodecName          string `json:"codec_name,omitempty"`
+		CodecLongName      string `json:"codec_long_name,omitempty"`
+		Profile            string `json:"profile,omitempty"`
+		CodecType          string `json:"codec_type"`
+		CodecTagString     string `json:"codec_tag_string"`
+		CodecTag           string `json:"codec_tag"`
+		Width              int    `json:"width,omitempty"`
+		Height             int    `json:"height,omitempty"`
+		CodedWidth         int    `json:"coded_width,omitempty"`
+		CodedHeight        int    `json:"coded_height,omitempty"`
+		ClosedCaptions     int    `json:"closed_captions,omitempty"`
+		FilmGrain          int    `json:"film_grain,omitempty"`
+		HasBFrames         int    `json:"has_b_frames,omitempty"`
+		SampleAspectRatio  string `json:"sample_aspect_ratio,omitempty"`
+		DisplayAspectRatio string `json:"display_aspect_ratio,omitempty"`
+		PixFmt             string `json:"pix_fmt,omitempty"`
+		Level              int    `json:"level,omitempty"`
+		ColorRange         string `json:"color_range,omitempty"`
+		ColorSpace         string `json:"color_space,omitempty"`
+		ColorTransfer      string `json:"color_transfer,omitempty"`
+		ColorPrimaries     string `json:"color_primaries,omitempty"`
+		ChromaLocation     string `json:"chroma_location,omitempty"`
+		FieldOrder         string `json:"field_order,omitempty"`
+		Refs               int    `json:"refs,omitempty"`
+		IsAvc              string `json:"is_avc,omitempty"`
+		NalLengthSize      string `json:"nal_length_size,omitempty"`
+		ID                 string `json:"id"`
+		RFrameRate         string `json:"r_frame_rate"`
+		AvgFrameRate       string `json:"avg_frame_rate"`
+		TimeBase           string `json:"time_base"`
+		StartPts           int    `json:"start_pts"`
+		StartTime          string `json:"start_time"`
+		DurationTs         int    `json:"duration_ts"`
+		Duration           string `json:"duration"`
+		BitRate            string `json:"bit_rate,omitempty"`
+		BitsPerRawSample   string `json:"bits_per_raw_sample,omitempty"`
+		NbFrames           string `json:"nb_frames"`
+		ExtradataSize      int    `json:"extradata_size"`
+		Disposition        struct {
+			Default         int `json:"default"`
+			Dub             int `json:"dub"`
+			Original        int `json:"original"`
+			Comment         int `json:"comment"`
+			Lyrics          int `json:"lyrics"`
+			Karaoke         int `json:"karaoke"`
+			Forced          int `json:"forced"`
+			HearingImpaired int `json:"hearing_impaired"`
+			VisualImpaired  int `json:"visual_impaired"`
+			CleanEffects    int `json:"clean_effects"`
+			AttachedPic     int `json:"attached_pic"`
+			TimedThumbnails int `json:"timed_thumbnails"`
+			NonDiegetic     int `json:"non_diegetic"`
+			Captions        int `json:"captions"`
+			Descriptions    int `json:"descriptions"`
+			Metadata        int `json:"metadata"`
+			Dependent       int `json:"dependent"`
+			StillImage      int `json:"still_image"`
+		} `json:"disposition"`
+		Tags struct {
+			Language    string `json:"language"`
+			HandlerName string `json:"handler_name"`
+			VendorID    string `json:"vendor_id"`
+			Encoder     string `json:"encoder"`
+			Timecode    string `json:"timecode"`
+		} `json:"tags,omitempty"`
+		SampleFmt      string `json:"sample_fmt,omitempty"`
+		SampleRate     string `json:"sample_rate,omitempty"`
+		Channels       int    `json:"channels,omitempty"`
+		ChannelLayout  string `json:"channel_layout,omitempty"`
+		BitsPerSample  int    `json:"bits_per_sample,omitempty"`
+		InitialPadding int    `json:"initial_padding,omitempty"`
+		Tags0          struct {
+			Language    string `json:"language"`
+			HandlerName string `json:"handler_name"`
+			VendorID    string `json:"vendor_id"`
+		} `json:"tags,omitempty"`
+		Tags1 struct {
+			Language    string `json:"language"`
+			HandlerName string `json:"handler_name"`
+			Timecode    string `json:"timecode"`
+		} `json:"tags,omitempty"`
+	} `json:"streams"`
+}
+
+func getVideoAspectRatio(filePath string) (string, error) {
+	cmd := exec.Command("ffprobe", "-v", "error", "-print_format", "json", "-show_streams", filePath)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Unmarshal the json in the bytes array to make it real JSON
+	// Place to hold the eventual JSON data
+	var ffprobeStreamData FfprobeSteam
+	//
+	err = json.Unmarshal(out.Bytes(), &ffprobeStreamData)
+	if err != nil {
+		return "", err
+	}
+	//
+	fileWidth := ffprobeStreamData.Streams[0].Width
+	fileHeight := ffprobeStreamData.Streams[0].Height
+	//
+	if fileWidth > fileHeight {
+		return "16:9", nil
+	}
+	if fileWidth < fileHeight {
+		return "9:16", nil
+	}
+	//
+	return "other", nil
 }
